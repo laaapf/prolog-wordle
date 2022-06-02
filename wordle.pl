7e2d67e6d08c929edd0ca92d061d01c38d0c2668
Lucas <lucaspfonseca98@gmail.com>
2022-06-01 23:35:14 -0300

    change

--- wordle.pl
+++ wordle.pl
@@ -17,12 +17,17 @@ game_setup(Number) :-
 
 
 play_game(Number, Tries, Random_word) :-
-  Tries_left is Tries - 1,
   write('Digite a palavra que deseja adivinhar: '),
   read(Guess),
   atom_chars(Guess,Guess_char_list),
+  Tries_left is Tries - 1,
   (Tries_left is 0 -> end_game();
   nl, play_game(Number,Tries_left,Random_word)).
+
+lenght_word_check(Number,Guess_char_list) :-
+	write(" ").
+	
+	
 
 end_game :-
     write("O jogo terminou! Deseja jogar novamente? (sim/nao) "),
@@ -34,10 +39,10 @@ end_game :-
     end_game())).
 
 get_random_word(Number, Random_word) :-
-	open('words.txt', read, Str),
-    read_file(Str,Words),
-    close(Str),
-    write(Lines),nl,
+	%open('words.txt', read, Str),
+    %read_file(Str,Words),
+    %close(Str),
+    %write(Lines),nl,
     Random_word = 'cacilda'.
 
                                                                            
