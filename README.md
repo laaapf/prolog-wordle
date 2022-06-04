3adc5f437964a22e5b6613140975f92706c5853a
Vitor Bardasson <vbardasson@id.uff.br>
2022-06-04 02:03:20 -0300

    Implementando escolha randômica de palavra

---
+++ README.md
@@ -0,0 +1,10 @@
+TODOs:
+automatizar o sorteio das palavras (FEITO)
+checar se letra está contida na palavra
+checar se palavra está no índice certo
+verificar caso em que letra aparece + de 1 vez na palavra
+fazer output bonitinho
+
+IDEIA: separar arquivos pelo tamanho da palavra
+
+COMANDO PARA EXECUÇÃO: swipl -s wordle.pl -t main
--- wordle.pl
+++ wordle.pl
@@ -4,30 +4,50 @@
 :- set_prolog_stack(local,  limit(2 000 000)).  % limit environment space
 
 main :-
-    print('Digite o tamanho da palavra que deseja jogar (4 a 7 letras): '),
-    read(Number),
-	(integer(Number),between(Number) -> game_setup(Number);
-    print("Não é um número válido!"),
-    nl,
-    main()).
+    get_random_word(Lines, N, Word),
+    write(Word).
+
+    %print('Digite o tamanho da palavra que deseja jogar (4 a 7 letras): '),
+    %read(Number),
+	%(integer(Number),between(Number) -> game_setup(Number);
+    %print("Não é um número válido!"),
+    %nl,
+    %main()).
 
 game_setup(Number) :-
-  	get_random_word(Number,Random_word),
-    play_game(Number,6, Random_word).
+  	get_random_word(Number, Random_word),
+    play_game(Number, 6, Random_word).
 
 
 play_game(Number, Tries, Random_word) :-
   write('Digite a palavra que deseja adivinhar: '),
   read(Guess),
-  atom_chars(Guess,Guess_char_list),
-  Tries_left is Tries - 1,
+  atom_chars(Guess, Guess_char_list),
+  Tries_left is Tries - 1,  
   (Tries_left is 0 -> end_game();
-  nl, play_game(Number,Tries_left,Random_word)).
+  nl, play_game(Number, Tries_left, Random_word)).
 
-lenght_word_check(Number,Guess_char_list) :-
+lenght_word_check(Number, Guess_char_list) :-
 	write(" ").
 	
-	
+get_random_word(Lines, N, Word) :-
+    random(0, 10309, N),
+    get_all_words(Lines),
+    nth1(N, Lines, Word).
+
+get_all_words(Lines) :-
+    open('words.txt', read, Str),
+    read_file(Str,Lines),
+    close(Str),
+    nl.
+
+read_file(Stream,[]) :-
+    at_end_of_stream(Stream).
+
+read_file(Stream,[X|L]) :-
+    \+ at_end_of_stream(Stream), % \+ not provable
+    read(Stream,X),
+    read_file(Stream,L).
 
 end_game :-
     write("O jogo terminou! Deseja jogar novamente? (sim/nao) "),
@@ -38,14 +58,6 @@ end_game :-
     nl,
     end_game())).
 
-get_random_word(Number, Random_word) :-
-	%open('words.txt', read, Str),
-    %read_file(Str,Words),
-    %close(Str),
-    %write(Lines),nl,
-    Random_word = 'cacilda'.
-
-                                                                           
 between(Number) :-
 	4 =< Number, 7 >= Number.
                   
