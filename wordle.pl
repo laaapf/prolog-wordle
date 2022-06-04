689035e42bbfa1bfa38079d3667d6d792db60229
laaapf <lucaspfonseca98@gmail.com>
2022-06-04 17:51:28 -0300

    reading from different files

--- wordle.pl
+++ wordle.pl
@@ -4,18 +4,16 @@
 :- set_prolog_stack(local,  limit(2 000 000)).  % limit environment space
 
 main :-
-    get_random_word(Lines, N, Word),
-    write(Word).
-
-    %print('Digite o tamanho da palavra que deseja jogar (4 a 7 letras): '),
-    %read(Number),
-	%(integer(Number),between(Number) -> game_setup(Number);
-    %print("Não é um número válido!"),
-    %nl,
-    %main()).
+    print('Digite o tamanho da palavra que deseja jogar (4 a 7 letras): '),
+    read(Number),
+	(integer(Number),between(Number) -> game_setup(Number);
+    print("Não é um número válido!"),
+    nl,
+    main()).
 
 game_setup(Number) :-
-  	get_random_word(Number, Random_word),
+  	get_random_word(Number,Lines, Random_word),
+    write(Random_word),
     play_game(Number, 6, Random_word).
 
 
@@ -30,13 +28,15 @@ play_game(Number, Tries, Random_word) :-
 lenght_word_check(Number, Guess_char_list) :-
 	write(" ").
 	
-get_random_word(Lines, N, Word) :-
-    random(0, 10309, N),
-    get_all_words(Lines),
-    nth1(N, Lines, Word).
+get_random_word(Number, Lines, Random_word) :-
+    get_all_words(Number,Lines,N),
+    nth1(N, Lines, Random_word).
 
-get_all_words(Lines) :-
-    open('words.txt', read, Str),
+get_all_words(Number,Lines,N) :-
+    (4 = Number -> open('4_letters.txt',read, Str),random(0, 972, N),write('1');
+    5 = Number -> open('5_letters.txt',read, Str),random(0, 2193, N),write('2');
+    6 = Number -> open('6_letters.txt',read, Str),random(0, 3042, N),write('3');
+    7 = Number -> open('7_letters.txt',read, Str),random(0, 4101, N),write('4'))->
     read_file(Str,Lines),
     close(Str),
     nl.
