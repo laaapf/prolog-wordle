364724d57b4ae9545add871f43e73e08f6229498
laaapf <lucaspfonseca98@gmail.com>
2022-06-08 20:05:47 -0300

    checking if word is valid

--- wordle.pl
+++ wordle.pl
@@ -3,37 +3,51 @@
 :- set_prolog_stack(global, limit(8 000 000)).  % limit term space (8Mb)
 :- set_prolog_stack(local,  limit(2 000 000)).  % limit environment space
 
+%verifica se o numero digitado pelo usuario eh valido
 main :-
-    print('Digite o tamanho da palavra que deseja jogar (4 a 7 letras): '),
+    print("Digite o tamanho da palavra que deseja jogar de 4 a 7 letras (EXEMPLO: 4.): "),
     read(Number),
 	(integer(Number),between(Number) -> game_setup(Number);
-    print("Não é um número válido!"),
+    print("Não eh um numero valido!"),
     nl,
     main()).
 
+%setup para iniciar o jogo
 game_setup(Number) :-
-  	get_random_word(Number,Lines, Random_word),
-    play_game(Number, 6, Random_word).
+  	get_random_word(Number,Lines, Random_word_aux),
+    downcase_atom(Random_word_aux,Random_word),
+    write(Random_word),
+    play_game(Number, 6, Random_word, Lines).
 
-
-play_game(Number, Tries, Random_word) :-
+%pega a entrada do usuario, verifica se a entrada eh valida, se for, continua o jogo, senao, uma palavra valida deve ser digitada novamente
+play_game(Number, Tries, Random_word, Lines) :-
   write("Faltam "),write(Tries),write(" chances!"),
-  nl,
-  write('Digite a palavra que deseja adivinhar: '),
-  read(Guess),
-  atom_chars(Guess, Guess_char_list),
+  get_guess(Guess_aux,Guess,Guess_char_list),
   lenght_word(Guess_char_list,Lenght),
-  Number \== Lenght -> write('O tamanho da palavra que você escreveu não corresponde com o tamanho da palavra aleatória que está jogando!'),nl,play_game(Number,Tries,Random_word);
-  Tries_left is Tries - 1,  (Tries_left is 0 -> end_game(); nl, play_game(Number, Tries_left, Random_word)).
+  check_if_guess_is_valid(Number,Lenght,Lines,Guess) -> 
+  Tries_left is Tries - 1,  (Tries_left is 0 -> end_game();nl, play_game(Number, Tries_left, Random_word, Lines));
+  write("A palavra que foi digitada eh invalida ou seu tamanho nao corresponde com o tamanho da palavra aleatoria que esta jogando!"),nl,play_game(Number,Tries,Random_word,Lines).
+
+%lendo a entrada do usuario
+get_guess(_Guess_aux,Guess,Guess_char_list) :-
+  nl,
+  write('Digite a palavra que deseja adivinhar entre aspas com um ponto no final(EXEMPLO: "teste".): '),
+  read(Guess_aux),
+  nl,
+  downcase_atom(Guess_aux,Guess),
+  atom_chars(Guess, Guess_char_list).
 
 
+%verifica o tamanho de uma palavra
 lenght_word([],Lenght) :- Lenght is 0. 
-lenght_word([H|T], Lenght) :- lenght_word(T,L),Lenght is L+1.
-	
+lenght_word([_H|T], Lenght) :- lenght_word(T,L),Lenght is L+1.
+
+%resgata uma palavra aleatoria de acordo com o arquivo correspondente
 get_random_word(Number, Lines, Random_word) :-
     get_all_words(Number,Lines,N),
     nth1(N, Lines, Random_word).
 
+%abertura e leitura do arquivo de palavras
 get_all_words(Number,Lines,N) :-
     (4 = Number -> open('4_letters.txt',read, Str),random(0, 972, N);
     5 = Number -> open('5_letters.txt',read, Str),random(0, 2193, N);
@@ -43,25 +57,41 @@ get_all_words(Number,Lines,N) :-
     close(Str),
     nl.
 
+%verificando se a palavra eh valida
+check_if_guess_is_valid(Number,Lenght,Lines,Guess):-
+    Number = Lenght,
+    check_if_guess_exists(Lines,Guess).
+
+%verifica se a palavra existe no arquivo de palavras
+check_if_guess_exists([], _Guess) :-
+    false.
+check_if_guess_exists([Element_aux|Lines], Guess) :-
+    downcase_atom(Element_aux,Element),
+    Element \== Guess -> check_if_guess_exists(Lines,Guess);true.
+
+%leitura do arquivo de palavras
 read_file(Stream,[]) :-
     at_end_of_stream(Stream).
-
 read_file(Stream,[X|L]) :-
     \+ at_end_of_stream(Stream), % \+ not provable
     read(Stream,X),
     read_file(Stream,L).
 
+%verificando se o usuario quer continuar o jogo ou finalizar o programa
 end_game :-
     nl,
-    write("O jogo terminou! Deseja jogar novamente? (sim/nao) "),
+    write("O jogo terminou! Deseja jogar novamente? (Digite "sim". ou "nao".) "),
    	read(Option),
-    (Option = 'sim' ->  main();
-    (Option = 'nao' -> write('Terminando o jogo!');
-    write("Opção inválida!"),
+    (Option = "sim" ->  nl,main();
+    (Option = "nao" -> write('Terminando o jogo!');
+    write("Opcao invalida!"),
     nl,
     end_game())).
 
+%checando se o tamanho da palavra eh valido
 between(Number) :-
 	4 =< Number, 7 >= Number.
                   
+    
+
     
