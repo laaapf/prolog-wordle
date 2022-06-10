8a7e50b1794695ddf4c0c44592c86d3b85d5f8bb
laaapf <lucaspfonseca98@gmail.com>
2022-06-10 14:43:40 -0300

    adding comments and color text

--- wordle.pl
+++ wordle.pl
@@ -1,10 +1,11 @@
 
 
-%verifica se o numero digitado pelo usuario eh valido
+%se o numero digitado pelo usuario eh valido, comeca o jogo
 main :-
     read_number(Number) -> game_setup(Number);
     print("Nao eh um numero valido!"),nl,main().
 
+%le o numero do usuario e realiza a verificacao
 read_number(Number) :-
     write("Digite o tamanho da palavra que deseja jogar de 4 a 7 letras (EXEMPLO: 4.): "),
     read(Number),
@@ -27,28 +28,33 @@ play_game(Number, Tries, Random_word, Lines,Random_word_char_list) :-
   check_char_guess_positions(Guess_char_list,Guess,Random_word,Random_word_char_list),Tries_left is Tries - 1,  (Tries_left is 0 -> end_game();nl, play_game(Number, Tries_left, Random_word, Lines,Random_word_char_list));
   write("A palavra que foi digitada eh invalida ou seu tamanho nao corresponde com o tamanho da palavra aleatoria que esta jogando!"),nl,play_game(Number,Tries,Random_word,Lines,Random_word_char_list).
 
+%verificando se o usuario acertou a palavra, se nao acertou, fazemos a verificacao dos caracteres
 check_char_guess_positions(Guess_char_list,_Guess,Random_word,Random_word_char_list) :-
-    Guess_char_list = Random_word_char_list -> write('Voce ganhou! A palavra eh "'),write(Random_word),write('"!'),end_game();
+    Guess_char_list = Random_word_char_list -> nl,ansi_format([bold,fg(green)],'Voce ganhou! A palavra eh "~w"!',[Random_word]),end_game();
     check_correct_positions(Guess_char_list,Random_word_char_list,1,Random_word_char_list,Chars_left),
     check_wrong_positions(Guess_char_list,Random_word_char_list,Random_word_char_list,1,Chars_left).
 
+%verifica as letras que nao estao na palavra e as letras que estao fora de posicao
 check_wrong_positions([],[],_Random_word_char_list,_Pos,_Chars_left):- !.
 check_wrong_positions([HG|TG],[HR|TR],Random_word_char_list,Pos,Chars_left):-
-    HG \== HR -> (member(HG,Chars_left) -> (remove_from_list(HG,Chars_left,Result),
-    nl,write('A letra "'),write(HG),write('" na posicao '),write(Pos),write(' existe na palavra mas esta na posicao errada '),
+    HG \== HR -> (member(HG,Chars_left) -> (remove_from_list(HG,Chars_left,Result), %percorremos a palavra aleatoria e o palpite do usuario ao mesmo tempo,
+    %verificamos se o head de cada palavra eh diferente, se for, verificamos se a letra atual do palpite pertence a lista de letras da palavra aleatoria que faltam ser
+    %verificadas, se fizer parte, subtraimos a primeira ocorrencia dessa letra na lista de caracteres que faltam ser verificados.
+    %caso a letra nao faca parte dessa lista de caracteres, a letra nao faz parte da palavra
+    nl,ansi_format([bold,fg(yellow)],'A letra "~w" na posicao ~w existe na palavra mas esta na posicao errada',[HG,Pos]),
     add_number(Pos,P),check_wrong_positions(TG,TR,Random_word_char_list,P,Result));
-    nl,write('A letra "'),write(HG),write('" na posicao '),write(Pos),write(' nao esta na palavra '),
+    nl,ansi_format([bold,fg(red)],'A letra "~w" na posicao ~w nao esta na palavra',[HG,Pos]),
     add_number(Pos,P),check_wrong_positions(TG,TR,Random_word_char_list,P,Chars_left));
     add_number(Pos,P),check_wrong_positions(TG,TR,Random_word_char_list,P,Chars_left).
                                                                      
-    
+%verificando as letras que estao na posicao correta
 check_correct_positions([],[],_Pos,Aux_list,Chars_left):- append([],Aux_list,Chars_left).
 check_correct_positions([HG|TG],[HR|TR],Pos,Aux_list,Chars_left) :-
-    HG = HR -> (nl,write('A letra "'),write(HG),write('" esta correta na posicao '),write(Pos),
-    remove_from_list(HG,Aux_list,Result),
-    add_number(Pos,P),write(Result),
-    check_correct_positions(TG,TR,P,Result,Chars_left));
-    add_number(Pos,P),check_correct_positions(TG,TR,P,Aux_list,Chars_left).
+    HG = HR -> (nl,ansi_format([bold,fg(green)],'A letra "~w" esta correta na posicao ~w',[HG,Pos]),
+    remove_from_list(HG,Aux_list,Result),     %se a letra estiver na posicao correta, removemos a primeira ocorrencia da letra na lista auxiliar de palavras
+    add_number(Pos,P),
+    check_correct_positions(TG,TR,P,Result,Chars_left)); 
+    add_number(Pos,P),check_correct_positions(TG,TR,P,Aux_list,Chars_left). %senao apenas continuamos
 
 
 %lendo a entrada do usuario
@@ -57,9 +63,10 @@ get_guess(_Guess_aux,Guess,Guess_char_list) :-
   write('Digite a palavra que deseja adivinhar entre aspas com um ponto no final(EXEMPLO: "teste".): '),
   read(Guess_aux),
   nl,
-  downcase_atom(Guess_aux,Guess),
-  atom_chars(Guess, Guess_char_list).
+  downcase_atom(Guess_aux,Guess),   %colocando a entrada do usuario em lower case
+  atom_chars(Guess, Guess_char_list).  %transformando a entrada do usuario em uma lista de caracteres
 
+%remove a primeira ocorrencia de um elemento de uma lista
 remove_from_list(_,[],[]).
 remove_from_list(Element, [Element|T],T).
 remove_from_list(Element, [H|T], [H|Result]) :- remove_from_list(Element,T,Result).
@@ -68,7 +75,7 @@ remove_from_list(Element, [H|T], [H|Result]) :- remove_from_list(Element,T,Resul
 add_number(N, Number) :-
     Number is N + 1.
 
-%verifica o tamanho de uma palavra
+%descobre o tamanho de uma palavra
 lenght_word([],Lenght) :- Lenght is 0. 
 lenght_word([_H|T], Lenght) :- lenght_word(T,L),Lenght is L+1.
 
@@ -77,7 +84,7 @@ get_random_word(Number, Lines, Random_word) :-
     get_all_words(Number,Lines,N),
     nth1(N, Lines, Random_word).
 
-%abertura e leitura do arquivo de palavras
+%abertura e leitura do arquivo de palavras, resgatando todas as palavras do arquivo
 get_all_words(Number,Lines,N) :-
     (4 = Number -> open('4_letters.txt',read, Str),random(0, 972, N);
     5 = Number -> open('5_letters.txt',read, Str),random(0, 2193, N);
@@ -89,7 +96,7 @@ get_all_words(Number,Lines,N) :-
 
 %verificando se a palavra eh valida
 check_if_guess_is_valid(Number,Lenght,Lines,Guess):-
-    Number = Lenght,
+    Number = Lenght,   %verificando se o tamanho da palavra corresponde ao tamanho que esta sendo jogado
     check_if_guess_exists(Lines,Guess).
 
 %verifica se a palavra existe no arquivo de palavras
@@ -114,12 +121,12 @@ end_game :-
     write('O jogo terminou! Deseja jogar novamente? (Digite "sim". ou "nao".) '),
    	read(Option),
     (Option = "sim" ->  nl,main();
-    (Option = "nao" -> write('Terminando o jogo!'),fail;
+    (Option = "nao" -> write('Terminando o jogo!'),halt(0);
     write("Opcao invalida!"),
     nl,
     end_game())).
 
-%checando se o tamanho da palavra eh valido
+%checando se o numero informado para jogar eh valido
 between(Number) :-
 	4 =< Number, 7 >= Number.
 
